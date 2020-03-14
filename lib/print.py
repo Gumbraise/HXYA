86c79d508d20cffd83dcaedd48b829333a13548f
Kellis <vikiclashonyt@gmail.com>
2020-03-14 23:36:06 +0100

    Added print.py

---
+++ lib/print.py
@@ -0,0 +1,221 @@
+from colorama import init, Fore, Back, Style
+
+mainMenu = """
+ ╔═════════════Welcome to HXYA═════════════╗               %s.-----...........---. %s
+ ║                                         ║               %s/`  ;:%s           `/ / %s
+ ║   %s1%s. Instagram                          ║          %s/`       ::%s        `/     :`%s
+ ║   %s2%s. YouTube                            ║      %s/`           ;:%s     /;        :`%s
+ ║   %s3%s. CCG                                ║   %s/`              :;%s`/             :`%s
+ ║                                         ║  %s:                %s;:              -/%s
+ ║   Options:                              ║  %s:            /:  %s::           -/ %s
+ ║   %s91%s. Configure keys...                 ║  %s:      /:        %s;:        -/     %s
+ ║   %s92%s. Clear Cache                       ║   %s/ ;/            %s::    ./        %s
+ ║                                         ║  %s.------.......------%s./             %s
+ ║   Window Options:                       ║
+ ║   %sC%s. Clear                              ║
+ ║   %sQ%s. Quit                               ║
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ╚══════════GitHub.com/Gumbraise═══════════╝
+        """ % (
+        Fore.BLACK, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLACK, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, 
+        Style.BRIGHT, Style.RESET_ALL
+    )
+
+youtubeMenu = """
+ ╔═════════════════YouTube═════════════════╗               %s.-----...........---. %s
+ ║                                         ║               %s/`  ;:%s           `/ / %s
+ ║   %s1%s. LikeClose                          ║          %s/`       ::%s        `/     :`%s
+ ║   %s2%s. DislikeClose                       ║      %s/`           ;:%s     /;        :`%s
+ ║   %s3%s. SubClose                           ║   %s/`              :;%s`/             :`%s
+ ║   %s4%s. CommentClose                       ║  %s:                %s;:              -/%s
+ ║   %s5%s. ViewClose                          ║  %s:            /:  %s::           -/ %s
+ ║                                         ║  %s:      /:        %s;:        -/     %s
+ ║   Options:                              ║   %s/ ;/            %s::    ./        %s
+ ║   %s91%s. Configure keys...                 ║  %s.------.......------%s./             %s
+ ║                                         ║
+ ║   Window Options:                       ║
+ ║   %sC%s. Clear                              ║
+ ║   %sB%s. Back                               ║
+ ║   %sQ%s. Quit                               ║
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ╚══════════GitHub.com/Gumbraise═══════════╝
+        """ % (
+        Fore.BLACK, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLACK, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, 
+        Style.BRIGHT, Style.RESET_ALL, 
+        Style.BRIGHT, Style.RESET_ALL
+    )
+
+instagramMenu = """
+ ╔════════════════Instagram════════════════╗               %s.-----...........---. %s
+ ║                                         ║               %s/`  ;:%s           `/ / %s
+ ║   %s1%s. Available Pseudos                  ║          %s/`       ::%s        `/     :`%s
+ ║   %s2%s. Message Bomber                     ║      %s/`           ;:%s     /;        :`%s
+ ║   %s3%s. Who Doesn't Follow Me              ║   %s/`              :;%s`/             :`%s
+ ║   %s4%s. Unfollow                           ║  %s:                %s;:              -/%s
+ ║                                         ║  %s:            /:  %s::           -/ %s
+ ║   Options:                              ║  %s:      /:        %s;:        -/     %s
+ ║   %s91%s. Configure keys...                 ║   %s/ ;/            %s::    ./        %s
+ ║                                         ║  %s.------.......------%s./             %s
+ ║   Window Options:                       ║
+ ║   %sC%s. Clear                              ║
+ ║   %sB%s. Back                               ║
+ ║   %sQ%s. Quit                               ║
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ╚══════════GitHub.com/Gumbraise═══════════╝
+        """ % (
+        Fore.BLACK, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLACK, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL,
+        Style.BRIGHT, Style.RESET_ALL,
+        Style.BRIGHT, Style.RESET_ALL,
+    )
+
+ccgMenu = """
+ ╔═══════════════════CCG═══════════════════╗               %s.-----...........---. %s
+ ║                                         ║               %s/`  ;:%s           `/ / %s
+ ║   %s1%s. Visa                               ║          %s/`       ::%s        `/     :`%s
+ ║   %s2%s. MasterCard                         ║      %s/`           ;:%s     /;        :`%s
+ ║                                         ║   %s/`              :;%s`/             :`%s
+ ║   Window Options:                       ║  %s:                %s;:              -/%s
+ ║   %sC%s. Clear                              ║  %s:            /:  %s::           -/ %s
+ ║   %sB%s. Back                               ║  %s:      /:        %s;:        -/     %s
+ ║   %sQ%s. Quit                               ║   %s/ ;/            %s::    ./        %s
+ ║                                         ║  %s.------.......------%s./             %s
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ╚══════════GitHub.com/Gumbraise═══════════╝
+        """ % (
+        Fore.BLACK, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+        Fore.BLACK, Fore.RED, Fore.RESET
+    )
+
+configYoutube = """
+ ╔═════════════════YouTube═════════════════╗               %s.-----...........---. %s
+ ║                                         ║               %s/`  ;:%s           `/ / %s
+ ║   %s1%s. API_KEY                            ║          %s/`       ::%s        `/     :`%s
+ ║                                         ║      %s/`           ;:%s     /;        :`%s
+ ║   Window Options:                       ║   %s/`              :;%s`/             :`%s
+ ║   %sC%s. Clear                              ║  %s:                %s;:              -/%s
+ ║   %sB%s. Back                               ║  %s:            /:  %s::           -/ %s
+ ║   %sQ%s. Quit                               ║  %s:      /:        %s;:        -/     %s
+ ║                                         ║   %s/ ;/            %s::    ./        %s
+ ║                                         ║  %s.------.......------%s./             %s
+ ║                                         ║
+ ║                                         ║
+ ╚══════════GitHub.com/Gumbraise═══════════╝
+        """ % (
+                Fore.BLACK, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLACK, Fore.RED, Fore.RESET
+            )
+
+configMenu = """
+ ╔═════════════════Config══════════════════╗               %s.-----...........---. %s
+ ║                                         ║               %s/`  ;:%s           `/ / %s
+ ║   %s1%s. Instagram                          ║          %s/`       ::%s        `/     :`%s
+ ║   %s2%s. YouTube                            ║      %s/`           ;:%s     /;        :`%s
+ ║                                         ║   %s/`              :;%s`/             :`%s
+ ║   Window Options:                       ║  %s:                %s;:              -/%s
+ ║   %sC%s. Clear                              ║  %s:            /:  %s::           -/ %s
+ ║   %sB%s. Back                               ║  %s:      /:        %s;:        -/     %s
+ ║   %sQ%s. Quit                               ║   %s/ ;/            %s::    ./        %s
+ ║                                         ║  %s.------.......------%s./             %s
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ╚══════════GitHub.com/Gumbraise═══════════╝
+        """ % (
+                Fore.BLACK, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLACK, Fore.RED, Fore.RESET
+            )
+
+configInstagram = """
+ ╔════════════════Instagram════════════════╗               %s.-----...........---. %s
+ ║                                         ║               %s/`  ;:%s           `/ / %s
+ ║   %s1%s. Username                           ║          %s/`       ::%s        `/     :`%s
+ ║   %s2%s. Password                           ║      %s/`           ;:%s     /;        :`%s
+ ║                                         ║   %s/`              :;%s`/             :`%s
+ ║   Window Options:                       ║  %s:                %s;:              -/%s
+ ║   %sC%s. Clear                              ║  %s:            /:  %s::           -/ %s
+ ║   %sB%s. Back                               ║  %s:      /:        %s;:        -/     %s
+ ║   %sQ%s. Quit                               ║   %s/ ;/            %s::    ./        %s
+ ║                                         ║  %s.------.......------%s./             %s
+ ║                                         ║
+ ║                                         ║
+ ║                                         ║
+ ╚══════════GitHub.com/Gumbraise═══════════╝
+        """ % (
+                Fore.BLACK, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Style.BRIGHT, Style.RESET_ALL, Fore.BLUE, Fore.RED, Fore.RESET,
+                Fore.BLACK, Fore.RED, Fore.RESET
+            )
